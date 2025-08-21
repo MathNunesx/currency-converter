667c4a2b08bb2eb8266fd420daafb2d7371626ff
Matheus Rodrigues <MATHEUSRO@dpc.ad>
2025-08-21 14:57:56 -0300

    Testye

---
+++ script.js
@@ -0,0 +1,6 @@
+const valor = document.getElementById('amount');
+const firstHolder = document.getElementById('firstHolder');
+const secondHolder = document.getElementById('secondHolder');
+const btn = document.getElementById('btn');
+
+
