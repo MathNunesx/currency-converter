5dc6dbbc94cf890be510c1861bc2de27653b42ef
Matheus Nunes <matheusjobs04@gmail.com>
2025-08-30 00:26:11 -0300

    Função readAmount | formatMoney e funcionalidade do icone de troca de conversão das opções

--- script.js
+++ script.js
@@ -2,5 +2,26 @@ const valor = document.getElementById('amount');
 const firstHolder = document.getElementById('firstHolder');
 const secondHolder = document.getElementById('secondHolder');
 const btn = document.getElementById('btn');
+const icon = document.getElementById('icon');
+const resultado = document.getElementById('result');
+
+function readAmount(){
+    return parseFloat(String(valor.value).replace(',', '.'))
+}
+
+function formatMoney(value, currency){
+    return new Intl.NumberFormat('pt-BR', {
+        style: 'currency', currency
+    }).format(value)
+}
+
+icon.addEventListener('click', () =>{
+    const tmp = firstHolder.value;
+    firstHolder.value = secondHolder.value
+    secondHolder.value
+
+    if(readAmount() > 0) converter()
+})
+
 
 
